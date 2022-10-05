cb904501f0d205e3df181536fdd50237a52888fc
Venus DM <crhowe90@gmail.com>
2022-10-05 09:52:22 -0500

    gallery and nav

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import photo from "../../assets/small/commercial/0.jpg";
+
+function Gallery (props) {
+    const currentCategory = {
+        name:"commercial",
+        descriprtion: "Photos of grocery stores, food trucks, and other commercial products",
+    };
+    return(
+        <section>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div>
+                <img src={photo} 
+                alt ="Commercial Example"
+                className="img-thumbnail mx-1"
+                />
+            </div>
+        </section>
+    );
+}
+
+export default Gallery;
